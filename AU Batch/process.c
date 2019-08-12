38aa749c5c8d0ac028b3f5173e9241a3d2fe70ea
Sujasri <31526296+Sujasri@users.noreply.github.com>
2019-08-12 13:12:14 -0500

    Add files via upload

---
+++ AU Batch/process.c
@@ -0,0 +1,29 @@
+
+/*
+ * This program will be launched in fork_execv
+ 
+ */
+#include <stdio.h> 
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+  int i,arg1,arg2;
+  /*char arg[3];
+  printf("A process is running ...\n");
+  argc--;
+  strcpy(arg[1],*argv[1]);
+  strcpy(arg[2],*argv[2]);*/
+ 
+  if (argc >1 ){
+    
+	/*sscanf(arg1, "%s", &arg[1]);
+	sscanf(arg2, "%s", &arg[2]);*/
+	for(i=0;i<10;i++){
+		for(i=0;i<50;i++){
+		
+		}
+		}
+		sleep(2);
+ return 0;
+}
+}
